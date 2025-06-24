70fe29a4a9dbfc99defc4b711a1d470eb24ee880
vedantlbhatt <vedubhatt2006@gmail.com>
2025-06-24 19:28:15 +0200

    Initial Commit

---
+++ Track My/Track_MyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Track_MyApp.swift
+//  Track My
+//
+//  Created by Vedant Bhatt on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Track_MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Track MyTests/Track_MyTests.swift
@@ -0,0 +1,17 @@
+//
+//  Track_MyTests.swift
+//  Track MyTests
+//
+//  Created by Vedant Bhatt on 6/24/25.
+//
+
+import Testing
+@testable import Track_My
+
+struct Track_MyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
