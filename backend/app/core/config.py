e9a6fd0b030a9cbd6d2d8a9a3d6afc91184ac65b
Rohan Nair <rohanmanunair@gmail.com>
2025-07-05 17:08:32 -0400

    Added constants file to backend, updated dependencies

---
+++ backend/app/core/config.py
@@ -0,0 +1,17 @@
+import os
+
+class Settings:
+    PROJECT_NAME: str = "TrackMy"
+    API_V1_STR: str = "/api/v1"
+    
+    MYSQL_USER: str = os.getenv("MYSQL_USER")
+    MYSQL_PASSWORD: str = os.getenv("MYSQL_PASSWORD")
+    MYSQL_DB: str = os.getenv("MYSQL_DATABASE")
+    MYSQL_HOST: str = os.getenv("MYSQL_HOST", "db")
+    MYSQL_PORT: int = int(os.getenv("MYSQL_PORT", 3306))
+
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
+    )
+
+settings = Settings()
