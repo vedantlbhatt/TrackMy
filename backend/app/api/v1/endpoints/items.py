8b6f2b699930ff71da2962e98125d89f9f9dbdf0
Rohan Nair <rohanmanunair@gmail.com>
2025-07-21 00:01:59 -0400

    Added new endpoint for getting items by user

--- backend/app/api/v1/endpoints/items.py
+++ backend/app/api/v1/endpoints/items.py
@@ -1,3 +1,20 @@
-from fastapi import APIRouter, Path
+from fastapi import APIRouter, Depends
+from app.services.db import item_store
+from app.services.db import user_store
+from app.core.init_db import SessionLocal
+from app.models.user import User
+from app.models.item import Item
 
-router = APIRouter(prefix="/users/{user_id}/items", tags=["items"])
+router = APIRouter()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.get("getItemByUser/{user_id}{item_id}")
+def get_user_by_id(user_id: int, item_id: int):
+    return item_store.get_item_by_user(user_id, item_id)
+    
--- backend/app/api/v1/endpoints/users.py
+++ backend/app/api/v1/endpoints/users.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
-from app.services.db import create_user
-from app.services.db impport get_user_by_id # Change this to be classes
+from app.services.db import item_store
+from app.services.db import user_store
 from app.core.init_db import SessionLocal
 from app.models.user import User
 
@@ -15,8 +15,8 @@ def get_db():
 
 @router.post("/addUser/")
 def add_user(email: str, user_name: str, payment_source: str, db=Depends(get_db)):
-    return create_user(db, email, user_name, payment_source)
+    return user_store.create_user(db, email, user_name, payment_source)
 
 @router.get("getUser/{user_id}")
 def get_user(user_id: int):
-    return get_user_by_id(user_id, db=Depends(get_db))
+    return user_store.get_user_by_id(user_id, db=Depends(get_db))
