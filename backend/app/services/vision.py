4ea0a2bc5387439f8c07c09ca6d294b66bacad12
Rohan Nair <rohanmanunair@gmail.com>
2025-07-23 12:42:12 -0700

    Added index logic, untested currently

--- backend/app/services/vision.py
+++ backend/app/services/vision.py
@@ -7,14 +7,26 @@ import numpy as np
 # Load CLIP model
 model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
-index = faiss.IndexFlatL2
+
+    
+def init_index():
+   index = faiss.read_index("/Users/rohannair/Desktop/Projects/TrackMy/backend/faiss.index")
+    if index.ntotal == 0:
+        index = faiss.IndexFlatL2(512) 
+    return index
+def write_to_index():
+    faiss.write_index(index, "/Users/rohannair/Desktop/Projects/TrackMy/backend/faiss.index")
 # Function to embed image
 def embed_image(image_url):
+    index = init_index()
     image = Image.open(image_url)
     inputs = processor(images=image, return_tensors="pt")
     with torch.no_grad():
         outputs = model.get_image_features(**inputs)
     index.add(np.expand_dims(outputs[0].numpy(), axis=0))
+    write_to_index()
+    
 def get_image_comp(image_url):
+    index = init_index()
     query_embedding = get_embedding(image_url)
     D, I = index.search(np.expand_dims(query_embedding, axis=0), k=5)
