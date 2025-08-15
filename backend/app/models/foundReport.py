a372f33dffb0b07d0c818f26fb1dbca58407c70e
vedantlbhatt <vedubhatt2006@gmail.com>
2025-08-15 19:11:09 -0400

    Trying to include functionality for adding images

--- backend/app/models/foundReport.py
+++ backend/app/models/foundReport.py
@@ -5,7 +5,7 @@ from app.core.database import Base
 class FoundReport(Base):
     __tablename__ = "found_reports"
     found_report_id = Column(Integer, primary_key = True, index = True, nullable = False)
-    founder_id = Column(Integer, primary_key = False, index = True, nullable = False) # tracks person who found the item
+    founder_id = Column(Integer, ForeignKey("users.user_id"), index = True, nullable = False) # tracks person who found the item
     found_item_id = Column(Integer, ForeignKey("items.item_id"), index = True, nullable = False)
     found_report_description = Column(VARCHAR(1000), nullable = True)
     longitude = Column(DECIMAL(8,6), nullable = True)
@@ -13,7 +13,7 @@ class FoundReport(Base):
     radius = Column(Integer, nullable = True)
     request_bounty = Column(DECIMAL(6,2), nullable = True) # case where item is found before report and founder asks for bounty
     
-    
+    user = relationship("User", back_populates="found_reports")
     item = relationship("Item", back_populates="found_reports")
     founder = relationship("User", back_populates="found_reports") # reference to user who found the item
-    images = relationship("Image", back_populates="found_report") # if we want to add images in future
+    #images = relationship("Image", back_populates="found_report") # if we want to add images in future
