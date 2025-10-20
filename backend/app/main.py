5adc6812deee50a79e9bdf1c304030f07903d762
vedantlbhatt <vedubhatt2006@gmail.com>
2025-10-20 16:18:04 -0400

    pushing for railway to connect for backend hosting

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from app.api import api_router
 from app.core.config import settings
@@ -9,9 +10,6 @@ app = FastAPI(title=settings.PROJECT_NAME)
 def read_root():
     return {"message": "Track My API is running", "version": "1.0"}
 
-for route in app.routes:
-    print(route.path, route.methods)
-
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
 app.add_middleware(
@@ -21,4 +19,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-# api_router.include_router(items.router, prefix="/items", tags=["items"]) need to change to follow (person_id/item i think)
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
